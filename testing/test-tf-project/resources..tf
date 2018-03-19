28811480b4fd43f05c03f7e8ea61e7394b698f6d
Chris Roberts <awschrisroberts@gmail.com>
2018-03-19 09:40:20 -0500

    cr

--- testing/test-tf-project/resources..tf
+++ testing/test-tf-project/resources..tf
@@ -19,24 +19,24 @@ terraform {
 
 
 module "fw_infra" {
-    source = "github.com/BDHostedSolutions/BD-HS//testing//tf-module"
-    resource_group_name             = "${var.resource_group_name}"
-    location                        = "${var.location}"
-    vnet_name                       = "${var.vnet_name}"
-    address_space                   = "${var.address_space}"
-    mgmt_subnet                     = "${var.mgmt_subnet}"
-    untrust_subnet                  = "${var.untrust_subnet}"
-    trust_subnet                    = "${var.trust_subnet}"
-    dns_server                      = "${var.dns_server}"
-    global_dns_server               = "${var.global_dns_server}"
-    storage_acct_name               = "${var.storage_acct_name}"
-    fwavs_name                      = "${var.fwavs_name}"
-    firewall_name                   = "${var.firewall_name}"
-    fw_username                     = "${var.fw_username}"
-    fw_password                     = "${var.fw_password}"
+    source              = "github.com/BDHostedSolutions/BD-HS//testing//tf-module"
+    resource_group_name = "${var.resource_group_name}"
+    location            = "${var.location}"
+    vnet_name           = "${var.vnet_name}"
+    address_space       = "${var.address_space}"
+    mgmt_subnet         = "${var.mgmt_subnet}"
+    untrust_subnet      = "${var.untrust_subnet}"
+    trust_subnet        = "${var.trust_subnet}"
+    dns_server          = "${var.dns_server}"
+    global_dns_server   = "${var.global_dns_server}"
+    storage_acct_name   = "${var.storage_acct_name}"
+    fwavs_name          = "${var.fwavs_name}"
+    firewall_name       = "${var.firewall_name}"
+    fw_username         = "${var.fw_username}"
+    fw_password         = "${var.fw_password}"
 }
 
 module "ilb" {
-    source = "github.com/BDHostedSolutions/BD-HS//testing//tf-module"
-    ilb_name                        = "${var.ilb_name}"
+    source   = "github.com/BDHostedSolutions/BD-HS//testing//tf-module"
+    ilb_name = "${var.ilb_name}"
 }
