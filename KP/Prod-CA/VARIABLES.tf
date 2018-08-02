be058715eb1c82a0800797c88a761f641d0d72ac
Chris Roberts <chris.roberts@bd.com>
2018-08-02 09:18:34 -0500

    cr

--- KP/Prod-CA/VARIABLES.tf
+++ KP/Prod-CA/VARIABLES.tf
@@ -1,6 +1,6 @@
 /* Configure Azure Provider and declare all the Variables that will be used in Terraform configurations */
 provider "azurerm" {
-  version         = "~> 1.7"
+  version         = "~> 1.11"
   subscription_id = "${var.subscription_id}"
   client_id       = "${var.client_id}"
   client_secret   = "${var.client_secret}"
